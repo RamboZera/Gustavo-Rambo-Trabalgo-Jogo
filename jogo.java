fb5d60ae66271b80dddf2cba34524bbbe1334dd9
Gustavo Rambo <145402749+RamboZera@users.noreply.github.com>
2024-05-20 19:27:23 -0300

    jogo.java
    ..

--- jogo.java
+++ jogo.java
@@ -25,47 +25,125 @@ Zona de Gás: Uma zona de gás tóxico encolhe continuamente o mapa, forçando o
 
 
 
-class Jogador {
-    String nome;
-    int saude;
-    int municoes;
-}
-class Equipe {
-    Jogador[] membros;
-}
+Classe Jogador
 
-class Veiculo {
-    String tipo;
-    int velocidade;
-}
-class Carro extends Veiculo {
-}
+    public class Jogador {
+    private String nome;
+    private int saude;
+    private int armadura;
+    private Arma armaPrimaria;
+    private Arma armaSecundaria;
+    private List<Equipamento> equipamentos;
 
-class Helicoptero extends Veiculo {
-}
-class Arma {
-    String nome;
-    int dano;
-}
-
-class Item {
-    String nome;
-}
+    public Jogador(String nome) {
+        this.nome = nome;
+        this.saude = 100;
+        this.armadura = 0;
+        this.equipamentos = new ArrayList<>();
+    }
+    public String getNome() { return nome; }
+    public int getSaude() { return saude; }
+    public void setSaude(int saude) { this.saude = saude; }
+    public int getArmadura() { return armadura; }
+    public void setArmadura(int armadura) { this.armadura = armadura; }
+    public void equiparArmaPrimaria(Arma arma) { this.armaPrimaria = arma; }
+    public void equiparArmaSecundaria(Arma arma) { this.armaSecundaria = arma; }
+    public void adicionarEquipamento(Equipamento equipamento) { this.equipamentos.add(equipamento); }
 
 
-class Mapa {
-    String[] locais;
-    
-}
+Classe Equipe
+
+  public class Equipe {
+    private String nome;
+    private List<Jogador> jogadores;
+
+    public Equipe(String nome) {
+        this.nome = nome;
+        this.jogadores = new ArrayList<>();
+    }
+
+    public void adicionarJogador(Jogador jogador) {
+        jogadores.add(jogador);
+    }
+
+    public List<Jogador> getJogadores() {
+        return jogadores;
+    }
+
+Classe Mapa
+
+public class Mapa {
+    private String nome;
+    private int largura;
+    private int altura;
+    private ZonaSegura zonaSegura;
+
+    public Mapa(String nome, int largura, int altura) {
+        this.nome = nome;
+        this.largura = largura;
+        this.altura = altura;
+        this.zonaSegura = new ZonaSegura(largura / 2, altura / 2, Math.min(largura, altura) / 2);
+    }
+
+    public ZonaSegura getZonaSegura() {
+        return zonaSegura;
+    }
+
+    public void atualizarZonaSegura() {
+
+Classe Arma
+
+public class Arma {
+    private String nome;
+    private int dano;
+    private int municao;
+
+    public Arma(String nome, int dano, int municao) {
+        this.nome = nome;
+        this.dano = dano;
+        this.municao = municao;
+    }
 
 
-class HUD {
-    Jogador jogador;
-    String[] informacoes;
-}
+Classe Equipamento
 
-class MenuInventario {
-    Jogador jogador;
-    Arma[] armas;
-    Item[] itens;
-}
+public class Equipamento {
+    private String nome;
+    private String tipo;
+
+    public Equipamento(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+
+Classe Veículo
+
+public class Veículo {
+    private String nome;
+    private int velocidade;
+    private int durabilidade;
+
+    public Veículo(String nome, int velocidade, int durabilidade) {
+        this.nome = nome;
+        this.velocidade = velocidade;
+        this.durabilidade = durabilidade;
+    }
+
+Classe ZonaSegura
+
+public class ZonaSegura {
+    private int centroX;
+    private int centroY;
+    private int raio;
+
+    public ZonaSegura(int centroX, int centroY, int raio) {
+        this.centroX = centroX;
+        this.centroY = centroY;
+        this.raio = raio;
+    }
+
+    public void diminuirRaio(int quantidade) {
+        this.raio -= quantidade;
+        if (this.raio < 0) this.raio = 0;
+    }
